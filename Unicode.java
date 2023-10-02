d0176c0de508018b00b46296265da688170c9806
Yahya Iddris Fatah <123132207+balatwist@users.noreply.github.com>
2023-10-02 17:25:19 -0500

    new

---
+++ Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        String name = "\u0048\u0069 \uD83D\uDC4B, \u006D\u0079 \u006E\u0061\u006D\u0065 \u0069\u0073  \u0393\u03B1\u03C7\u03C5\u03B1";
+
+
+        System.out.println(name);
+        System.out.println("Size of string = "+ name.length());
+    }
+}
